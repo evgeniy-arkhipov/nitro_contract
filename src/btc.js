ef634919cc8b7d924e4ac3d36522bdeade11f391
n0cte <ilnur.a.g@gmail.com>
2017-10-20 18:04:28 +0300

    listeners from btc to eth-token

--- src/btc.js
+++ src/btc.js
@@ -1,5 +1,7 @@
 const rpc = require('./rpc/bitcoin');
+
 const addr = require('./config').btcCompanyAddr;
+
 const socket = require('socket.io-client')(rpc.host);
 const kue = require('kue');
 
@@ -9,11 +11,11 @@ socket.on('connection', console.log.bind(console, 'connected'));
 socket.on('connect', socket.emit.bind(socket,'subscribe', 'inv'));
 
 socket.on('tx', tx => {
-  const tout = tx.vout.find(out => !!out[addr])
-  if(!!!tout){
+  const vout = tx.vout.find(out => !!out[addr])
+  if(!!!vout){
     return;
   }
   queue
-    .create('tx', { id: tx.txid })
+    .create('tx', { id: tx.txid, value: parseInt(vout[addr]) })
     .save();
 });
--- src/rpc/contract.js
+++ src/rpc/contract.js
@@ -2,6 +2,55 @@ const ethereum = require('./ethereum');
 const abi = require('../config/abi');
 const config = require('../config');
 
-const TokenContract = new ethereum.eth.Contract(abi, config.contractAddress);
+const address = config.contractAddress;
+const account = ethereum.eth.accounts.privateKeyToAccount(config.contractOwner);
+const contract = new ethereum.eth.Contract(abi, address);
 
-module.exports = TokenContract;
+const method = contract.methods.sendBounty(account.address, 2000)
+
+const fromOwner = (methodName, ...args) => {
+  const method = contract.methods[methodName](...args);
+  return method
+    .estimateGas()
+    .then(
+      gas =>
+        account
+          .signTransaction({
+            to: address,
+            gasLimit: gas,
+            data: method.encodeABI()
+          })
+    )
+    .then(
+      ({rawTransaction}) =>
+        ethereum
+          .eth
+          .sendSignedTransaction(rawTransaction)
+    )
+}
+
+Object.assign(exports, {
+  address,
+  account,
+  contract,
+
+  balanceOf(addr){
+    return contract.methods.balanceOf(addr).call();
+  },
+
+  verified(addr){
+    return contract.methods.verified(addr).call();
+  },
+
+  verify(addr){
+    return fromOwner('verify', addr);
+  },
+
+  setLimit(ethLimit){
+    return fromOwner('setLimit', ethLimit);
+  },
+
+  funding(to, eth){
+    return fromOwner('funding', to, eth);
+  }
+});
